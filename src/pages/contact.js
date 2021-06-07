6443b6010035bce7710b0b227c17732ec6f00b84
nhutquang <nhutquang0909@gmail.com>
2021-06-07 16:48:20 +0700

    edit page contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import Layout from "../components/Layout"
+import * as styles from '../styles/contact.module.css'
 
 export default function Contact() {
     return (
         <Layout>
-            <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/success">
+            <div className={`wrapper ${styles.wrapper}`}>
+                <form className={`form ${styles.form}`} name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/success">
+                    <input type="hidden" name="bot-field" />
+                    <input type="hidden" name="form-name" value="contact" />
+                    <div className={`pageTitile ${styles.pageTitle} ${styles.title}` }>Contact Us </div>
+                    <div className={`secondaryTitle ${styles.secondaryTitle} ${styles.title}`}>Please fill this form to contact us.</div>
+                    <input type="text" htmlFor="name" className={`name ${styles.name} ${styles.formEntry}`} placeholder="Name" />
+                    <input type="text" htmlFor="email" className={`email ${styles.email} ${styles.formEntry}`} placeholder="Email"/>
+                    <textarea className={`message ${styles.message} ${styles.formEntry}`} placeholder="Message"></textarea>
+
+                    <input type="submit" className={`submit ${styles.submit} ${styles.formEntry}`} value="Send Message" />
+                    <input type="reset" className={`reset ${styles.reset} ${styles.formEntry}`} value="Clear" />
+                </form>
+            </div>
+
+
+
+            {/* <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/success">
                 <input type="hidden" name="bot-field" />
                 <input type="hidden" name="form-name" value="contact" />
-                <div className="field half first">
+                <div className="field-half first">
                     <label htmlFor="name">Name</label>
                     <input type="text" name="name" id="name" />
                 </div>
-                <div className="field half">
+                <div className="field-half">
                     <label htmlFor="email">Email</label>
                     <input type="text" name="email" id="email" />
                 </div>
@@ -27,7 +45,7 @@ export default function Contact() {
                         <input type="reset" value="Clear" />
                     </li>
                 </ul>
-            </form>
+            </form> */}
         </Layout>
     )
 }
--- src/pages/success.js
+++ src/pages/success.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from "../components/Layout"
+import {Link} from 'gatsby'
 
 export default function Success() {
     return (
@@ -7,6 +8,7 @@ export default function Success() {
             <div>
                 <h1>Success</h1>
                 <p>Thanks you for contact us</p>
+                <Link to="/contact">Go back side</Link>
             </div>
         </Layout>
     )
