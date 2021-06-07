506bda6f106fd645df36727a7ef0d4b7bb1f0193
nhutquang <nhutquang0909@gmail.com>
2021-06-07 17:00:42 +0700

    update page contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,11 +9,15 @@ export default function Contact() {
                 <form className={`form ${styles.form}`} name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/success">
                     <input type="hidden" name="bot-field" />
                     <input type="hidden" name="form-name" value="contact" />
+                    
                     <div className={`pageTitile ${styles.pageTitle} ${styles.title}` }>Contact Us </div>
                     <div className={`secondaryTitle ${styles.secondaryTitle} ${styles.title}`}>Please fill this form to contact us.</div>
-                    <input type="text" htmlFor="name" className={`name ${styles.name} ${styles.formEntry}`} placeholder="Name" />
-                    <input type="text" htmlFor="email" className={`email ${styles.email} ${styles.formEntry}`} placeholder="Email"/>
-                    <textarea className={`message ${styles.message} ${styles.formEntry}`} placeholder="Message"></textarea>
+
+                    <input type="text" name="name" id="name" htmlFor="name" className={`name ${styles.name} ${styles.formEntry}`} placeholder="Name" />
+
+                    <input type="text" name="email" id="email" htmlFor="email" className={`email ${styles.email} ${styles.formEntry}`} placeholder="Email"/>
+
+                    <textarea name="message" id="message" className={`message ${styles.message} ${styles.formEntry}`} placeholder="Message"></textarea>
 
                     <input type="submit" className={`submit ${styles.submit} ${styles.formEntry}`} value="Send Message" />
                     <input type="reset" className={`reset ${styles.reset} ${styles.formEntry}`} value="Clear" />
